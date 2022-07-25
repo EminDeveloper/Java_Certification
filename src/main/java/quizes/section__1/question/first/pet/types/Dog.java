4e4348b961fd7f4f66c73ccd9618638590993abc
Emin <emin.f.mammadov@gmail.com>
2022-07-25 16:19:59 +0400

    Create Project

---
+++ src/main/java/quizes/section__1/question/first/pet/types/Dog.java
@@ -0,0 +1,5 @@
+package quizes.section__1.question.first.pet.types;
+
+public class Dog {
+    public static String petType= "DOG";
+}
---
+++ src/main/java/quizes/section__1/question/first/pet/util/TestPet.java
@@ -0,0 +1,11 @@
+package quizes.section__1.question.first.pet.util;
+
+import static quizes.section__1.question.first.pet.types.Dog.*;
+
+public class TestPet{
+
+    public static void main(String[] args) {
+        System.out.println(petType);
+    }
+
+}
