758e404583420a8190b0ed69a9b34e8a637dab6f
Emin <emin.f.mammadov@gmail.com>
2022-08-02 11:10:28 +0400

    Local Variable Type Inference: Out of the Ordinary Concepts  Tutorial 25

---
+++ src/main/java/tutorials/section_3/localVariableTypeInterference24/com/WordyCompanyName/ALongPackageName/AClassWithAVeryLongName.java
@@ -0,0 +1,23 @@
+package tutorials.section_3.localVariableTypeInterference24.com.WordyCompanyName.ALongPackageName;
+
+public class AClassWithAVeryLongName {
+    String name;
+
+    public AClassWithAVeryLongName() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return "AClassWithAVeryLongName{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
