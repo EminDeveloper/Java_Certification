405b6d254de75a995427b5d7f40089ba98b39a99
Emin <emin.f.mammadov@gmail.com>
2022-08-03 15:36:22 +0400

    Update FooBar.java

--- src/main/java/tutorials/section_3/localVariableInitialization17/FooBar.java
+++ src/main/java/tutorials/section_3/localVariableInitialization17/FooBar.java
@@ -1,10 +1,4 @@
 package tutorials.section_3.localVariableInitialization17;
-/*
-Learn Programming Academy's Java 1Z0-815 Certification Exam Course
-Section 3: Working with Java Primitive Data Types and String APIs
-Topic: Declare and Initialize Variables
-Sub-Topic: Initialization of primitive data type variables - including default values
-*/
 
 public class FooBar {
     public static void main(String[] args) {
