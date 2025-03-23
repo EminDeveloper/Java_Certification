2a4e8212718f519f03b8f15064ba2defcb987554
EminDeveloper <emin.f.mammadov@gmail.com>
2025-03-23 18:24:32 +0400

    50. Hands-On Java Array Declaration, Initialization, and Basic Manipulation

---
+++ src/main/java/quizes/section_4/Quiz_4.java
@@ -0,0 +1,14 @@
+package quizes.section_4;
+
+public class Quiz_4 {
+    static int testVariable;
+
+    public static void main(String[] args) {
+        byte i, j;
+        for (i = 100, j = 0; i <= 300; i += 90, j++) { // Line 1
+            testVariable = i % 50;   //  Line 2
+            System.out.print(testVariable + " "); // Line 3
+        }
+        System.out.println(j);
+    }
+}
---
+++ src/main/java/quizes/section_4/Quiz_5.java
@@ -0,0 +1,12 @@
+//package quizes.section_4;
+//
+//public class Quiz_5 {
+//    static int testVariable;
+//
+//    public static void main(String[] args) {
+//        for (int i = 0; i++ < 10; i-- ; i += 1){
+//
+//            System.out.print(i++ + ",");
+//        }
+//    }
+//}
